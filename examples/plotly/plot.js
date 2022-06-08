5dcf293263bf1ef07f0a8898bba7d5a932870ddc
FedericoGarza <fede.garza.ramirez@gmail.com>
2022-06-08 17:17:06 -0500

    fix: remove bearer

--- examples/plotly/plot.js
+++ examples/plotly/plot.js
@@ -51,7 +51,7 @@ const options = {
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkRzcDUxYkp4UllkSXgxdkh3V3VRMyJ9.eyJpc3MiOiJodHRwczovL2Rldi00aGo1emVnay51cy5hdXRoMC5jb20vIiwic3ViIjoiNEoxUmhhNW1ZclV3OHpqQWFXVks2QTJKc2RXVFZNd0NAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vY3J5c3RhbC1iYWxsLWF1dGgwLmNvbSIsImlhdCI6MTY1MzUyMjYyNywiZXhwIjoxNjU2MTE0NjI3LCJhenAiOiI0SjFSaGE1bVlyVXc4empBYVdWSzZBMkpzZFdUVk13QyIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.WS8gWjaTPSCKSRHbVwSjWf-ArbfiS7epxPw0boY_tS9aUfKQexbm47Ys-rR2g7hmhjgLd2M0RREx0onQVN7fyBPnoND49ki48xWq99JSoy-NOxu6ZGZyccvEwtgurCpDZsbwjXo8gyrRqzH9iELoDbFlwqbX7IAPaq4DvottoHP0E_K6VjCovftP5c84GWDpvdSY_Dtlal1PvsHyQGVBZSVvpMHZ1rEiMdeF6kRaFOt6ASZUMjmwXo5LvJcpHrOKqVryG0ApNPNUXkfoAJ7G8jl18yWl7ekbgPLuf8CBCfFxA6ZevTqCgUWQfGPGR_BGqNHOOi4N-AzDJID1Jbrjyg'
+    Authorization: 'Bearer <your_api_key>'
   },
   body: JSON.stringify({
     timestamp: trace1.x,
