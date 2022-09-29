44a3ee22f119d7a25dedc1cc69a883adc848cb72
FedericoGarza <fede.garza.ramirez@gmail.com>
2022-09-28 20:39:24 -0500

    feat: add anomalies plot

--- examples/streamlit/app.py
+++ examples/streamlit/app.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def plot(df, df_forecast=None):
+def plot(df, df_forecast=None, df_anomaly=None):
 	figs = [
         go.Scatter(x=df['timestamp'], y=df['value'], 
                    mode='lines',
@@ -42,6 +42,14 @@ def plot(df, df_forecast=None):
 					   legendrank=4,
 					   marker=dict(color='#E7C4C0'),
 					   name='Forecast'),
+		]
+	if df_anomaly is not None:
+		figs += [
+			go.Scatter(x=df_anomaly['timestamp'],
+            		   y=df_anomaly['value'],
+                       mode='markers',
+                       marker=dict(color='red', size=15),
+                       name='Anomalies'),
 		]
 	fig = go.Figure(figs)
 	fig.update_layout({
@@ -79,8 +87,8 @@ You can contribute with your pre-trained models by following [this tutorial](htt
 """
 st.write(intro)
 
-file = 'https://raw.githubusercontent.com/Nixtla/transfer-learning-time-series/main/datasets/air_passengers.csv'
-df = pd.read_csv(file)
+file = 'https://raw.githubusercontent.com/Nixtla/transfer-learning-time-series/main/datasets/peyton_manning.csv'
+df = pd.read_csv(file).tail(300)
 
 # forecast code
 payload = dict(
@@ -97,9 +105,27 @@ headers = {
 response = requests.post('http://app.nixtla.io/forecast', json=payload, headers=headers)
 df_forecast = pd.DataFrame(json.loads(response.text))
 
+# anomalies code
+payload = dict(
+	timestamp=df['timestamp'].to_list(),
+	value=df['value'].to_list(),
+	level=99
+)
+headers = {
+	"Accept": "application/json",
+	"Content-Type": "application/json",
+	"Authorization": f"Bearer {os.environ['BEARER_STREAMLIT']}"
+	
+}
+response = requests.post('http://app.nixtla.io/anomaly_detector', json=payload, headers=headers)
+df_anomaly = pd.DataFrame(json.loads(response.text))
+
 # plot without forecast
 #st.plotly_chart(plot(df.tail(200)), use_container_width=True)
 
 # plot with forecast
-st.plotly_chart(plot(df.tail(200), df_forecast), use_container_width=True)
+#st.plotly_chart(plot(df.tail(200), df_forecast), use_container_width=True)
+
+# plot with forecast and anomalies
+st.plotly_chart(plot(df.tail(200), df_forecast, df_anomaly), use_container_width=True)
 
